6ba0629c523264740f85ba8869bd3a792531b0a5
shamsWMM <shams.alsaegh@gmail.com>
2024-12-30 13:24:40 +1100

    Add update and delete actions

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -19,7 +19,7 @@ public class StockController(ApplicationDBContext context) : ControllerBase
         return Ok(stocks);
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id:int}")]
     public IActionResult GetStock([FromRoute] int id)
     {
         var stock = _context.Stock.Find(id);
@@ -33,9 +33,43 @@ public class StockController(ApplicationDBContext context) : ControllerBase
     [HttpPost]
     public IActionResult CreateStock([FromBody] StockDto stockDto)
     {
-        Stock stock = stockDto.ToStock();
+        var stock = stockDto.ToStock();
         _context.Stock.Add(stock);
         _context.SaveChanges();
         return CreatedAtAction(nameof(GetStock), new { id = stock.Id }, stock.ToDto());
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateStock([FromRoute] int id, [FromBody] StockDto stockDto)
+    {
+        var stock = _context.Stock.Find(id);
+        if (stock == null)
+        {
+            return NotFound();
+        }
+
+        stock.Symbol = stockDto.Symbol;
+        stock.CompanyName = stockDto.CompanyName;
+        stock.Purchase = stockDto.Purchase;
+        stock.LastDiv = stockDto.LastDiv;
+        stock.Industry = stockDto.Industry;
+        stock.MarketCap = stockDto.MarketCap;
+
+        _context.SaveChanges();
+        return Ok(stock.ToDto());
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteStock([FromRoute] int id)
+    {
+        var stock = _context.Stock.Find(id);
+        if (stock == null)
+        {
+            return NotFound();
+        }
+
+        _context.Stock.Remove(stock);
+        _context.SaveChanges();
+        return NoContent();
     }
 }
